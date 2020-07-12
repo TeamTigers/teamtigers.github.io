01a4941894179a032d47e50c3c85b1ab9e1d1575
zubayerhimel <zhimel007@gmail.com>
2020-07-12 23:39:36 +0600

    updated project section

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -2,6 +2,7 @@ $(function () {
   $(".tabs").tabs();
 });
 
+AOS.init();
 document.addEventListener("DOMContentLoaded", function () {
   window.setTimeout(
     document.querySelector("#banner-svg").classList.add("animated"),
